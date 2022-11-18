3533ad5bc96048a2285964b38b390e9f5ed957f1
Bruce Wells <brucekwells@gmail.com>
2022-11-18 11:48:56 -0500

    Remove phpstan hack

--- src/PHPFUI/InstaDoc/Controller.php
+++ src/PHPFUI/InstaDoc/Controller.php
@@ -158,19 +158,16 @@ class Controller
 
 			$div = new \PHPFUI\GridX();
 			$div->add($section->getBreadCrumbs($fullClassName));
-
-			$cell = new \PHPFUI\Cell();
 			$div->add(' &nbsp; ');
 			$icon = new \PHPFUI\FAIcon('far', 'clipboard');
 			$icon->setToolTip('Send Constructor to Clipboard');
+			$div->add($icon);
 			$callout = new \PHPFUI\Callout('success');
 			$callout->add('Copied!');
 			$callout->addClass('small');
 			$parameters = $this->getConstructorParameters($fullClassName);
 			$parameters = \str_replace("\n", '', $parameters);
-			// @phpstan-ignore-next-line hack for now
 			$page->addCopyToClipboard("new \\{$fullClassName}({$parameters});", $icon, $callout);
-			$page->setDebug(1);
 			$div->add($callout);
 			$mainColumn->add($div);
 
